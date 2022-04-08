8a1e7a7c7eba93a00ffe269c7e1c0382c081fa41
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-04-08 16:27:32 +0200

    5

--- js/script.js
+++ js/script.js
@@ -24,27 +24,27 @@ const text = [
 
 const items = [
   {
-    immagine: "img/01.jpg",
+    immagine: "./img/01.jpg",
     title: "Svezia",
     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
   },
   {
-    immagine: "img/02.jpg",
+    immagine: "./img/02.jpg",
     title: "Svizzera",
     text: "Lorem ipsum",
   },
   {
-    immagine: "img/03.jpg",
+    immagine: "./img/03.jpg",
     title: "Gran Bretagna",
     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
   },
   {
-    immagine: "img/04.jpg",
+    immagine: "./img/04.jpg",
     title: "Germania",
     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
   },
   {
-    immagine: "img/05.jpg",
+    immagine: "./img/05.jpg",
     title: "Paradise",
     text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
   },
